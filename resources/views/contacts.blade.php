2081b5d045e17697eb279e49666aaff7fc4d3696
aleksmakarov5 <aleksmakarov5@yandex.ru>
2026-01-14 13:35:07 +0300

    -

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 @section('title')
-Контакты
+    Контакты
 @endsection
 @section('content')
     <div class="page ">
@@ -9,10 +9,14 @@
         <h2>ООО "МРТС Терминал"</h2>
         <p>163049, г. Архангельск, Дрейера,6.</p>
         <p>
-            <b>Телефоны:</b><br>
-            Приемная: +7(8182) 63-89-53<br>
-            Руководитель подразделения безопасности: +7(8182) 63-89-53 доб 403<br>
-            <b></b>
+            Генеральный директор:
+        <h4>Разов Александр Викторович</h4>
+        <br>
+        <b>Телефоны:</b><br>
+
+        Приемная: +7(8182) 63-89-53<br>
+        Руководитель подразделения безопасности: +7(8182) 63-89-53 доб 403<br>
+        <b></b>
         </p>
         <p>
             <b>E-mail: </b><a href="mailto:shemyakina_p@mrts.ru"
@@ -20,11 +24,12 @@
             <b>E-mail руководителя подразделения безопасности: </b><a href="mailto:glavatskii_v@mrts.ru"
                 mce_href="mailto:glavatskii_v@mrts.ru">glavatskii_v@mrts.ru</a><br mce_bogus="1">
         </p>
-        <p><b>WWW:</b> <a href="http://мртс-терминал.рф/"
-                mce_href="http://мртс-терминал.рф">мртс-терминал.рф</a><br mce_bogus="1"></p>
+        <p><b>WWW:</b> <a href="http://мртс-терминал.рф/" mce_href="http://мртс-терминал.рф">мртс-терминал.рф</a><br
+                mce_bogus="1"></p>
         <p>
             <script type="text/javascript" charset="utf-8" async
-                src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Aa6498732364b1bb3e1bb71002e5bc76dc86dac1af9df3f3e48f30373664be266&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
+                src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Aa6498732364b1bb3e1bb71002e5bc76dc86dac1af9df3f3e48f30373664be266&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=true">
+            </script>
         </p>
 
 
