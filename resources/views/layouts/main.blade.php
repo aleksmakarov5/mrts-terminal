c5d95fa05915ae237a57915771554b5c8d9b693a
aleksmakarov5 <aleksmakarov5@yandex.ru>
2026-09-24 06:28:01 +0300

    -

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -65,7 +65,7 @@
                         class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1" aria-label="Bootstrap">
                         <svg class="bi" width="30" height="24" aria-hidden="true">
                             <use xlink:href="#bootstrap"></use>
-                        </svg> </a> <span class="mb-3 mb-md-0 text-body-secondary">© 2025 ООО "МРТС Терминал"</span>
+                        </svg> </a> <span class="mb-3 mb-md-0 text-body-secondary">© 2026 ООО "МРТС Терминал"</span>
                 </div>
                 <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
                     <li class="ms-3"><a class="text-body-secondary" href="#" aria-label="Instagram"><svg
